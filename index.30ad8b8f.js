a7a22d83e961d0c5473d76f7137c8f900b6d76af
filip-bak <118935319+filip-bak@users.noreply.github.com>
2023-02-22 19:53:24 +0000

    Deploying to gh-pages from @ filip-bak/ice-cream-project@bf4443fb9ed15780ccb185ee54d97e88e713596e 🚀

--- index.30ad8b8f.js
+++ index.30ad8b8f.js
@@ -1,2 +1,2 @@
 (()=>{const o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.48dc5a9a.js.map
+//# sourceMappingURL=index.30ad8b8f.js.map
